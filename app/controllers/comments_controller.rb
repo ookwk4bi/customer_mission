ed8092081cc5e329a656a4a850fa31b1538ec5b9
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-08-10 23:41:23 +0900

    コントローラー定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
-end
+
+    def create
+      @comment = Comment.new(comment_params)
+      @comment.user_id = current_user.id
+      if @comment.save
+        redirect_to root_path
+      else
+        @customer = Customer.find(params[:customer_id])
+        redirect_to root_path
+      end
+    end
+  
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_to root_path
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:body, :score, :customer_id, :user_id)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :customers
+  resources :customers do
+    resources :comments, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
+  end
   root 'customers#index'
+  resources :comments, only: [:create, :destroy]
+  resources :documents do
+    collection { post :import }
+    collection do
+    delete 'destroy_all'
+  end
+end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
